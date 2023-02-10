2a15ec1512042d6dde7e5de26c1ac5d827e95744
Fatimah1403 <fattyhassan14@gmail.com>
2023-02-10 19:57:22 +0000

    Task 2, builtin contd..

--- shell.h
+++ shell.h
@@ -20,6 +20,7 @@
  * @argv: an array of strings generated from arg
  * @argc: the argument count
  * @err_num: the error code for exit()s
+ * @alias: the alias node
  */
 
 
@@ -30,6 +31,7 @@ typedef struct infodetails
 	char **argv;
 	int argc;
 	int err_num;
+	list_t *alias;
 
 } info_t;
 
@@ -46,6 +48,48 @@ typedef struct builtin
 	char *type;
 	int (*func)(info_t *);
 } builtin_table;
+
+
+/* prototypes for 0-atoi.c */
+int interactive(info_t *);
+int _isalphabet(int);
+int _atoi(char *);
+
+/* prototypes for builtin.c */
+
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
+
+/* prototypes for builtin1.c */
+int _myhistory(info_t *);
+int _myalias(info_t *);
+int set_alias(info_t *info, char *str);
+int unset_alias(info_t *info, char *str);
+int print_alias(list_t *node)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
