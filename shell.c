447e751eb391f09ae65ff158b455f1191a884084
JohnsonKayode <kayodejohnson01@gmail.com>
2023-02-10 14:37:26 +0100

    scripts for first simple shell

---
+++ shell.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	char *argv[]; //this should hold the input of user
+
+	while (1) // once the code runs, print the $
+	{
+		printf("$: "); 
+	//all our codes will be inside this bloc
+	//step 1.
+	//while the users input is not Ctrl D because ctrl D means end of program
+	//Do the function 
+	//if function is invalid perror (print error) and go back to print $.
+	//else do the function and still go back to $: 
+		
+	}
+}
