cc15aded6c4bfb2c0ca3d4ac67b4e8a77059964d
Fatimah1403 <fattyhassan14@gmail.com>
2023-02-10 18:14:36 +0000

    Task 1, builtin

--- shell.h
+++ shell.h
@@ -16,18 +16,36 @@
  * containing prototype and pointers.
  *
  * @readfd: the fd to read line input
+ * @arg: a string generated from getline containing arguements
+ * @argv: an array of strings generated from arg
+ * @argc: the argument count
+ * @err_num: the error code for exit()s
  */
 
 
 typedef struct infodetails
 {
 	int readfd;
+	char *arg;
+	char **argv;
+	int argc;
+	int err_num;
 
 } info_t;
 
 
 
 
+/**
+ *struct builtin - contains a builtin string and related function
+ *@type: the builtin command flag
+ *@func: the function
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
 
 
 
