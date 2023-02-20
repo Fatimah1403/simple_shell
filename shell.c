d80da98e93c3748007af07649ddb30e7147b2121
JohnsonKayode <kayodejohnson01@gmail.com>
2023-02-20 11:31:33 +0000

    Made sampleshell1

--- shell.c
+++ shell.c
@@ -4,17 +4,5 @@
 
 int main()
 {
-	char *argv[]; //this should hold the input of user
-
-	while (1) // once the code runs, print the $
-	{
-		printf("$: "); 
-	//all our codes will be inside this bloc
-	//step 1.
-	//while the users input is not Ctrl D because ctrl D means end of program
-	//Do the function 
-	//if function is invalid perror (print error) and go back to print $.
-	//else do the function and still go back to $: 
-		
-	}
+	char *argv[]; //this should hold the input o
 }
