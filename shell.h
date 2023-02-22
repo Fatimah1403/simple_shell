99f36ab3bf6edafde629c458e0cc290f033d2a26
Fatimah1403 <fattyhassan14@gmail.com>
2023-02-22 17:30:50 +0000

    getline of the project

--- shell.h
+++ shell.h
@@ -10,6 +10,7 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/wait.h>
 
 
 /* for command chaining */
@@ -64,11 +65,15 @@ typedef struct liststr
  * @history: the history node
  * @alias: the alias node
  * @env: linked list local copy of environ
- * environ: custom modified copy of environ from LL env
+ * @environ: custom modified copy of environ from LL env
  * @env_changed: on if environ was changed
  * @fname: the program filename
  * @line_count: the error count
- * @line_count: the error count
+ * @linecount_flag: if on count this line of input
+ * @history: the history node
+ * @histcount: the history line number count
+ * @cmd_buf: address of pointer to cmd_buf, on if chaining
+ * @cmd_buf_type: CMD_type ||, &&, ;
  */
 
 
@@ -87,9 +92,10 @@ typedef struct infodetails
 	int env_changed;
 	char *fname;
 	int linecount_flag;
-		unsigned int line_count;
-
-
+	unsigned int line_count;
+	int histcount;
+	char **cmd_buf; /* pointer to cmd ; chain buffer, for memory mangement */
+	int cmd_buf_type; /* CMD_type ||, &&, ; */
 
 } info_t;
 
@@ -113,8 +119,10 @@ typedef struct builtin
 } builtin_table;
 
 
-/* _exits.c */
+/* _exit.c */
 char *_strchr(char *, char);
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
 
 /* prototypes for 0-atoi.c */
 int interactive(info_t *);
@@ -142,16 +150,22 @@ int _putsfd(char *str, int fd);
 /*  string.c*/
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
+int _strlen(char *);
+char *_strcat(char *, char *);
 
 
 /*  string1.c*/
 void _puts(char *);
 int _putchar(char);
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
 
 /*  error1.c */
 int _erratoi(char *);
 void print_error(info_t *, char *);
 int print_d(int, int);
+void remove_comments(char *);
+
 
 
 /* _environ.c */
@@ -178,9 +192,33 @@ void free_list(list_t **);
 size_t print_list(const list_t *);
 ssize_t get_node_index(list_t *, list_t *);
 list_t *node_starts_with(list_t *, char *, char);
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+
+/* _getline.c */
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void sigintHandler(int);
 
 
+/* toem_realloc.c */
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+void *_realloc(void *, unsigned int, unsigned int);
 
+/* _history.c */
+int build_history_list(info_t *info, char *buf, int linecount);
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int renumber_history(info_t *info);
+
+/* _vars.c */
+int is_chain(info_t *, char *, size_t *);
+void check_chain(info_t *, char *, size_t *, size_t, size_t);
+int replace_alias(info_t *);
+int replace_vars(info_t *);
+int replace_string(char **, char *);
 
 
 
